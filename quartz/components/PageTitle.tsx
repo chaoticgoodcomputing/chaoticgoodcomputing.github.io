0261de0b9dd202195b68f979344700ca97992caf
Spencer Elkington <spelkington@gmail.com>
2026-01-02 23:46:47 -0700

    fix: icon

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { pathToRoot, joinSegments } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -6,9 +6,14 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/icon.png")
+
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <img src={iconPath} class="page-title-icon" alt="" />
+        <span>{title}</span>
+      </a>
     </h2>
   )
 }
@@ -18,6 +23,22 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+
+.page-title a {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  text-decoration: none;
+  color: inherit;
+}
+
+.page-title-icon {
+  width: 4rem;
+  height: 4rem;
+  border-radius: 0.25rem;
+  flex-shrink: 0;
+  object-fit: cover;
 }
 `
 
